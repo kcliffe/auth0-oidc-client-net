78ea989431fd1ebde5865e86538a040c051fb887
Kelly <kelly.cliffe@gmail.com>
2019-01-26 20:21:30 +1300

    added  customchrometabs

--- src/Auth0.OidcClient.Android/PlatformWebView.cs
+++ src/Auth0.OidcClient.Android/PlatformWebView.cs
@@ -1,14 +1,23 @@
 using Android.App;
 using Android.Content;
+using Android.Graphics;
+using Android.Support.CustomTabs;
 using IdentityModel.OidcClient.Browser;
 using System;
 using System.Threading.Tasks;
 
 namespace Auth0.OidcClient
 {
-
-    class PlatformWebView : IBrowser
+    public class PlatformWebView : IBrowser
     {
+        private readonly Activity _context;
+        private CustomTabsActivityManager _customTabs;
+
+        public ChromeCustomTabsWebView(Activity context)
+        {
+            _context = context;
+        }
+
         public Task<BrowserResult> InvokeAsync(BrowserOptions options)
         {
             if (string.IsNullOrWhiteSpace(options.StartUrl))
@@ -21,42 +30,51 @@ namespace Auth0.OidcClient
                 throw new ArgumentException("Missing EndUrl", nameof(options));
             }
 
-            // must be able to wait for the intent to be triggered to continue
+            // must be able to wait for the intent to be finished to continue
             // with setting the task result
-            var tcs = new TaskCompletionSource<BrowserResult>();
+            var _tcs = new TaskCompletionSource<BrowserResult>();
 
-            void Callback(string response)
+            // create & open chrome custom tab
+            _customTabs = new CustomTabsActivityManager(_context);
+
+            // build custom tab
+            var builder = new CustomTabsIntent.Builder(_customTabs.Session)
+               .SetToolbarColor(Color.Argb(255, 52, 152, 219))
+               .SetShowTitle(true)
+               .EnableUrlBarHiding();
+            
+            var customTabsIntent = builder.Build();
+
+            // ensures the intent is not kept in the history stack, which makes
+            // sure navigating away from it will close it
+            customTabsIntent.Intent.AddFlags(ActivityFlags.NoHistory);
+
+            ActivityMediator.MessageReceivedEventHandler callback = null;
+            callback = (response) =>
             {
                 // remove handler
-                ActivityMediator.Instance.ActivityMessageReceived -= Callback;
+                AndroidClientChromeCustomTabsApplication.Mediator.ActivityMessageReceived -= callback;
+
+                // start MainActivity (will close the custom tab)
+                //_context.StartActivity(typeof(MainActivity));
 
                 // set result
-                if (response == "UserCancel")
+                _tcs.SetResult(new BrowserResult
                 {
-                    tcs.SetResult(new BrowserResult { ResultType = BrowserResultType.UserCancel });
-                }
-                else
-                {
-                    tcs.SetResult(new BrowserResult
-                    {
-                        Response = response,
-                        ResultType = BrowserResultType.Success
-                    });
-                }
-            }
+                    Response = response,
+                    ResultType = BrowserResultType.Success
+                });
+            };
 
             // attach handler
-            ActivityMediator.Instance.ActivityMessageReceived += Callback;
+            AndroidClientChromeCustomTabsApplication.Mediator.ActivityMessageReceived += callback;     
 
-            // Launch browser
-            var uri = Android.Net.Uri.Parse(options.StartUrl);
-            var intent = new Intent(Intent.ActionView, uri);
-            intent.AddFlags(ActivityFlags.NoHistory)
-                .AddFlags(ActivityFlags.NewTask);
-            Application.Context.StartActivity(intent);
+            // launch
+            customTabsIntent.LaunchUrl(_context, Android.Net.Uri.Parse(options.StartUrl));
 
-            // Return task which will be completed when intent is triggered
-            return tcs.Task;
-        }
+            // need an intent to be triggered when browsing to the "io.identitymodel.native://callback"
+            // scheme/URI => CallbackInterceptorActivity
+            return _tcs.Task;
+        } 
     }
 }
